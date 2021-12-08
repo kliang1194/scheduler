478a12222ed8e67ea28cb1240362314ccf24ebee
Kyle <kliang1194@gmail.com>
2021-12-08 14:42:15 -0500

    Add code to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 import "./styles.scss";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
-export default function Appointment (props) {
+
+
+export default function Appointment(props) {
+
+    // const {student, interviewer} = props.interview
+
   return (
     <>
-    {!props.time && <article className="appointment">No Appointment</article>}
-    {props.time && <article className="appointment">Appointment at {props.time}</article>}
-
+    <Header time = {props.time} />
+    {props.interview ? <Show student = {props.interview["student"]} interviewer = {props.interview["interviewer"]}  /> : <Empty /> }
     </>
   );
-}
+};
--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+
 
 import "index.scss";
 
@@ -10,14 +11,14 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header.js";
-import Empty from "components/Appointment/Empty.js";
-import Show from "components/Appointment/Show.js";
-import Confirm from "components/Appointment/Confirm.js";
-import Status from "components/Appointment/Status.js"
-import Error from "components/Appointment/Error.js"
-import Form from "components/Appointment/Form.js"
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 storiesOf("Button", module)
   .addParameters({
@@ -188,4 +189,20 @@ storiesOf("Button", module)
         onSave={action("onSave")}
         onCancel={action("onCancel")}
         />
-      ));
+      ))
+      .add("Appointment Empty", () => (
+        <Fragment>
+          <Appointment id={1} time="4pm" />
+          <Appointment time="5pm" />
+        </Fragment>
+      ))
+      .add("Appointment Booked", () => (
+        <Fragment>
+          <Appointment
+            id={1}
+            time="4pm"
+            interview={{ student: "Lydia Miller-Jones", interviewer }}
+          />
+          <Appointment time="5pm" />
+        </Fragment>
+      ))
