9178dced66a8173c0d4f92f37051d725eda44558
Kyle <kliang1194@gmail.com>
2021-12-08 10:10:53 -0500

    Set up InterviewerList Component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+
+export default function InterviewerList () {
+
+  return (
+  <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list"></ul>
+</section>
+);
+
+}
