103ef8e6043654e65d64f465389b8a8dc3d789af
Kyle <kliang1194@gmail.com>
2021-12-08 10:51:38 -0500

    Refactor InterviewerList and InterviewerListItem Components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -12,7 +12,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={set => props.setInterviewer(interviewer.id)}
+        setInterviewer={(event) => props.setInterviewer(interviewer.id)}
       />
     );
   });
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,9 +10,8 @@ export default function InterviewerListItem(props) {
 
   return (
     <li
-      onClick={() => {
-        props.setInterviewer(props.id);
-      }}
+      onClick=
+        {props.setInterviewer}
       className={interviewerClass}>
       <img
         className="interviewers__item-image"
