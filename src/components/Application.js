c86b2b39b45200f8f61819577583cdece23c3c03
Kyle <kliang1194@gmail.com>
2021-12-09 16:43:59 -0500

    Manage state by combining the state of day, days, and appointments into a state into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,8 +54,14 @@ const appointmentArray = Object.values(appointments).map((appointment) => {
 })
 
 export default function Application(props) {
-  const [days, setDays] = useState([]); 
-  const [day, setDay] = useState('Monday');
+ const [state, setState] = useState({
+   day: "Monday",
+   days: [],
+   appointments: {}
+ });
+
+ const setDay = day => setState({ ...state, day });
+ const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     const daysURL = '/api/days';
@@ -75,8 +81,8 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
   <DayList
-  days={days}
-  value={day}
+  days={state.days}
+  value={state.day}
   onChange={setDay}
 />
 </nav>
