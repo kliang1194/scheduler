b6b97ae7fe1936e94faa35b8aa953d78ec6fe4ed
Kyle <kliang1194@gmail.com>
2022-01-09 19:31:46 -0500

    Update method used to set state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,19 +3,23 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+  const popHistory = () =>
+    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
 
-  function transition(newMode, replace) {
-    if (!replace) {
-      setHistory((prev) => [...prev, newMode]);
+  function transition(newMode, replace = false) {
+    if (replace) {
+      popHistory();
     }
     setMode(newMode);
+    setHistory((prev) => [...prev, newMode]);
   }
 
   function back() {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+    if (history.length === 1) {
+      return;
     }
+    setMode(history[history.length - 2]);
+    popHistory();
   }
 
   return { mode, transition, back };
